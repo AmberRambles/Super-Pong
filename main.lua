3b0c9246ad3fa72ef49eab9bfc2c2b0eedc4e235
Amber Shifflett <amber.z.shifflett@outlook.com>
2024-03-11 22:21:44 -0700

    Implements Paddle and Ball classes
    Includes the source files in load(). Creates two paddle instances and
    one ball. Current code should display all the items.

--- main.lua
+++ main.lua
@@ -2,13 +2,24 @@ function love.load()                     --called once at program load
     io.stdout:setvbuf("no")              --allows the attached console to print content during runtime
     require "src.helpers"                --my personal helper function file
     Object = require "src.classic"       --the classic package by rxi
+    require "src.paddle" --Paddle class
+    require "src.ball" --Ball class
     randomStartup()                      --primes the Random Number Generator
+    computerPaddle = Paddle(700, 200)
+    userPaddle = Paddle(50, 200)
+    gameBall = Ball(375, 150)
+    
 end
 
 function love.update(dt)                 --dt stands for delta time
-    --do stuff
+    computerPaddle:update(dt)
+    userPaddle:update(dt)
+    gameBall:update(dt)
 end
 
 function love.draw()                     --only rendering commands here
     love.graphics.print("Hello World", 400, 300)
+    computerPaddle:draw()
+    userPaddle:draw()
+    gameBall:draw()
 end
