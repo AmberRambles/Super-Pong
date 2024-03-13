ed910af4cad467e3e7f1cc71fcc0a5e09854a4af
Amber Shifflett <amber.z.shifflett@outlook.com>
2024-03-12 22:25:40 -0700

    Corrects logic for ball boundary checking

--- src/ball.lua
+++ src/ball.lua
@@ -6,29 +6,37 @@ function Ball:new(x,y)
     self.y = y
     self.speed = 100
     self.radius = 5
+    self.xMod = 0.5
+    self.yMod = 0.5
 end
 
 function Ball:yBoundCheck()
 	local yBound = love.graphics.getHeight()
-	if self.y + self.radius == 0 then
+	if (self.y - self.radius <= 0) then
 		--bounce() --from top bound
-	elseif self.y + self.radius == yBound then
+		print("Bounce top")
+	elseif (self.y + self.radius >= yBound) then
 		--bounce() --from bottom bound
+		print("Bounce bottom")
 	end
 end
 
 function Ball:xBoundCheck()
 	local xBound = love.graphics.getWidth()
-	if self.x + self.radius == 0 then
+	if (self.x - self.radius) <= 0 then
 		--point() --from left bound
-	elseif self.x + self.radius == xBound then
+		print("Point CPU")
+	elseif (self.x + self.radius >= xBound) then
 		--point() --from right bound
+		print("Point Player")
 	end
 end
 
 function Ball:update(dt)
-    self.xBoundCheck()
-    self.yBoundCheck()
+	self.x = self.x + (self.speed * self.xMod * dt)
+	self.y = self.y + (self.speed * self.yMod * dt)
+	self:xBoundCheck()
+	self:yBoundCheck()
 end
 
 function Ball:draw()
