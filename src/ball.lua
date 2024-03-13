652342f48894b8e8a3df8c01fe48e426f19c360b
Amber Shifflett <amber.z.shifflett@outlook.com>
2024-03-12 18:41:13 -0700

    Adds Ball methods for detecting touching the four window edges

--- src/ball.lua
+++ src/ball.lua
@@ -8,8 +8,27 @@ function Ball:new(x,y)
     self.radius = 5
 end
 
+function Ball:yBoundCheck()
+	local yBound = love.graphics.getHeight()
+	if self.y + self.radius == 0 then
+		--bounce() --from top bound
+	elseif self.y + self.radius == yBound then
+		--bounce() --from bottom bound
+	end
+end
+
+function Ball:xBoundCheck()
+	local xBound = love.graphics.getWidth()
+	if self.x + self.radius == 0 then
+		--point() --from left bound
+	elseif self.x + self.radius == xBound then
+		--point() --from right bound
+	end
+end
+
 function Ball:update(dt)
-    --self.x = self.x + (self.speed * dt)
+    self.xBoundCheck()
+    self.yBoundCheck()
 end
 
 function Ball:draw()
