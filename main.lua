27352a5d9172bab5cd23b07e852fca053ad70e55
Amber Shifflett <amber.z.shifflett@outlook.com>
2024-03-19 15:32:57 -0700

    Prints basic scoreboard

--- main.lua
+++ main.lua
@@ -90,6 +90,15 @@ end
 
 function love.draw()                     --only rendering commands here
     --love.graphics.print("Super Pong!", 400, 100)
+    local movingY = 0
+    local midScreen = love.graphics.getWidth() / 2
+    --prints center line
+    while (movingY < love.graphics.getHeight()) do
+	love.graphics.line(midScreen, movingY, midScreen, movingY + 5)
+	movingY = movingY + 25
+    end
+    love.graphics.print("" .. score.user, midScreen - 20, love.graphics.getHeight() / 10)
+    love.graphics.print("" .. score.computer, midScreen + 20, love.graphics.getHeight() / 10)
     computerPaddle:draw()
     userPaddle:draw()
     gameBall:draw()
